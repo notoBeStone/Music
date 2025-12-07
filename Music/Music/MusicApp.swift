412cefbe959764d8961946f246c09f6d755b9a44
peng.ruilin <peng.ruilin@glority.cn>
2025-12-08 00:13:09 +0800

    Init App

---
+++ Music/Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by 彭瑞淋 on 2025/12/8.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
